f2ba71a2ba92a3d109f07ba24b299821d3bc8c71
Gaston Coria <gaston.coria@uala.com.ar>
2026-06-18 09:22:34 -0300

    add profiler pprof

--- server.go
+++ server.go
@@ -9,6 +9,7 @@ import (
 	"log/slog"
 	"net"
 	"net/http"
+	"net/http/pprof"
 	"os"
 	"strconv"
 	"time"
@@ -60,6 +61,8 @@ func newServer(store store.Store, port int, cancel context.CancelFunc, logger *s
 	s.httpServer = srv
 
 	mux.Handle("GET /metrics", promhttp.Handler())
+	mux.Handle("GET /debug/pprof/", s.authMiddleware(http.HandlerFunc(pprof.Index)))
+	mux.Handle("GET /debug/pprof/profile", s.authMiddleware(http.HandlerFunc(pprof.Profile)))
 	mux.HandleFunc("GET /", s.handlerIndex)
 	mux.Handle("POST /api/login", s.authMiddleware(http.HandlerFunc(s.handlerLogin)))
 	mux.Handle("POST /api/shorten", s.authMiddleware(http.HandlerFunc(s.handlerShortenLink)))
